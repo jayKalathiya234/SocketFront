aeeae04c9f95cc7af727f7ffc3c58a48edcff829
jayKalathiya234 <jay.kalathiyainfotech123@gmail.com>
2025-04-18 16:09:29 +0530

    Update useSocket.js

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -24,7 +24,7 @@ const decryptMessage = (encryptedText) => {
   return result;
 };
 
-const SOCKET_SERVER_URL = "http://localhost:4000"; // Move to environment variable in production
+const SOCKET_SERVER_URL = "https://socketlive-gv9j.onrender.com/"; // Move to environment variable in production
 
 export const useSocket = (userId, localVideoRef, remoteVideoRef, allUsers) => {
   const [isConnected, setIsConnected] = useState(false);
